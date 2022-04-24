3498ae23831086fd139600b340ad60f8cdae9271
gchaparroc <gchaparroc.certificaciones@gmail.com>
2022-04-24 09:50:02 -0400

    segundo commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Aratoca Charala con express'));
+app.get('/', (req, res) => res.send('Aratoca Charala con express, cambio 2.0'));
 
 app.listen(3000);
 console.log('Servidor corriendo en puerto 3000');
